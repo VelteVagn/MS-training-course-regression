622a6bfa4353c42b3054e9c4b78264c77d2a5703
Vetle <vetle@tjora.net>
2025-09-03 15:30:28 +0200

    feat: add training functions

--- source.py
+++ source.py
@@ -31,6 +31,67 @@ def cost_function(actual, estimated):
 # Create a model to be trained
 model = RegModel()
 
+def train_one_iteration(inputs, true_temperatures, last_cost:float):
+    """
+    Runs one iteration of training.
+
+    Args:
+        inputs: dates for the model to predict based upon.
+
+        true_temperatures: actual temperatures.
+
+        last_cost (float): cost of prediction on previous training.
+
+    Out:
+        bool: whether training should continue or not.
+
+        float: cost after this iteration of training.
+    """
+
+    # get a prediction with current model
+    estimated_temperatures = model.predict(inputs)
+
+    # calculate the performance of the model
+    difference, cost = cost_function(true_temperatures, estimated_temperatures)
+
+    # decide whether training should continue
+    if cost >= last_cost:
+        # end training
+        return False, cost
+    else:
+        intercept_update, slope_update = optimiser.get_parameter_updates(inputs, cost, difference)
+
+        # update parameters
+        model.intercept += intercept_update
+        model.slope += slope_update
+
+        return True, cost
+
+def train(inputs, true_temperatures):
+    """Trains the model until optimal.
+
+    Args:
+        inputs: dates for the model to predict based upon.
+
+        true_temperatures: actual temperatures.
+
+    Out: 
+        None
+    """
+    last_cost = np.inf
+    i = 0
+    continue_loop = True
+
+    while continue_loop:
+        continue_loop, last_cost = train_one_iteration(inputs=data["years_since_1982"],
+                                                       true_temperatures=date["normalised_temperature"]
+                                                       last_cost=last_cost
+                                                    )
+        if i % 277 == 0:
+            print(f"iteration: {i}")
+
+        i += 1
+
 py_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py"
 code = requests.get(py_url)
 
