104255a46da0e1246697c4a4d8041f163039b133
Vetle <vetle@tjora.net>
2025-09-03 13:21:33 +0200

    feat: import optimiser

--- source.py
+++ source.py
@@ -4,9 +4,19 @@ import pandas as pd
 import requests
 import types
 
-data_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/seattleWeather_1948-2017.csv"
-
 py_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py"
+code = requests.get(py_url)
+
+# error handling
+if not code.ok:
+    error_msg = f"Fetching url failed. Status code: {code.status_code}"
+    raise RuntimeError(error_msg)
+
+# create module
+m0b_optimizer = types.ModuleType("m0b_optimizer")
+exec(code.text, m0b_optimizer.__dict__)
+
+data_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/seattleWeather_1948-2017.csv"
 
 # import the weather data
 data = pd.read_csv(data_url, parse_dates=["date"])
