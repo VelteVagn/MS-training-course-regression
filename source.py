0741f6cb0faca8d050abd92ff69609313f3dea33
Vetle <vetle@tjora.net>
2025-09-04 14:47:05 +0200

    chore: finalise script

--- source.py
+++ source.py
@@ -8,6 +8,15 @@ matplotlib.use("PDF")
 import matplotlib.pyplot as plt
 import numpy as np
 
+# import module from Microsoft
+py_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py"
+code = requests.get(py_url)
+if not code.ok: # handle errors
+    error_msg = f"Fetching url failed. Status code: {code.status_code}"
+    raise RuntimeError(error_msg) 
+m0b_optimizer = types.ModuleType("m0b_optimizer")
+exec(code.text, m0b_optimizer.__dict__)
+
 class RegModel:
     def __init__(self):
         """ Initiates an untrained regression model."""
@@ -31,6 +40,9 @@ def cost_function(actual, estimated):
 # Create a model to be trained
 model = RegModel()
 
+# create an optimiser
+optimiser = m0b_optimizer.MyOptimizer()
+
 def train_one_iteration(inputs, true_temperatures, last_cost:float):
     """
     Runs one iteration of training.
@@ -50,7 +62,7 @@ def train_one_iteration(inputs, true_temperatures, last_cost:float):
 
     # get a prediction with current model
     estimated_temperatures = model.predict(inputs)
-
+    
     # calculate the performance of the model
     difference, cost = cost_function(true_temperatures, estimated_temperatures)
 
@@ -62,8 +74,8 @@ def train_one_iteration(inputs, true_temperatures, last_cost:float):
         intercept_update, slope_update = optimiser.get_parameter_updates(inputs, cost, difference)
 
         # update parameters
-        model.intercept += intercept_update
-        model.slope += slope_update
+        model.intercept -= intercept_update
+        model.slope -= slope_update
 
         return True, cost
 
@@ -84,7 +96,7 @@ def train(inputs, true_temperatures):
 
     while continue_loop:
         continue_loop, last_cost = train_one_iteration(inputs=data["years_since_1982"],
-                                                       true_temperatures=date["normalised_temperature"]
+                                                       true_temperatures=data["normalised_temperature"],
                                                        last_cost=last_cost
                                                     )
         if i % 277 == 0:
@@ -92,47 +104,31 @@ def train(inputs, true_temperatures):
 
         i += 1
 
-py_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/m0b_optimizer.py"
-code = requests.get(py_url)
+if __name__ == "__main__":
+    # import the weather data
+    data_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/seattleWeather_1948-2017.csv"
+    data = pd.read_csv(data_url, parse_dates=["date"])
 
-# error handling
-if not code.ok:
-    error_msg = f"Fetching url failed. Status code: {code.status_code}"
-    raise RuntimeError(error_msg)
+    # filter out all months except January
+    data = data[[d.month == 1 for d in data.date]].copy()
 
-# create module
-m0b_optimizer = types.ModuleType("m0b_optimizer")
-exec(code.text, m0b_optimizer.__dict__)
+    # optimise data for ML:
+    # number of years since 1982
+    data["years_since_1982"] = [(d.year + d.timetuple().tm_yday / 365.25) - 1982 for d in data.date]
+    # normalise temperatures
+    data["normalised_temperature"] = (data["min_temperature"] - np.mean(data["min_temperature"])) / np.std(data["min_temperature"])
 
-# create an optimiser
-optimiser = m0b_optimizer.MyOptimizer()
+    # train the model
+    train(data["years_since_1982"], data["normalised_temperature"])
 
-data_url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/seattleWeather_1948-2017.csv"
+    # plot the data and model predictions
+    plt.scatter(data["years_since_1982"], data["normalised_temperature"], marker=".")
+    plt.plot(data["years_since_1982"], model.predict(data["years_since_1982"]), c="r")
 
-# import the weather data
-data = pd.read_csv(data_url, parse_dates=["date"])
+    # labels and legend
+    plt.xlabel("years_since_1982")
+    plt.ylabel("normalised_temperature")
+    plt.title("January Temperatures (Normalised)")
 
-# filter out all months except January
-data = data[[d.month == 1 for d in data.date]].copy()
-
-# normalise the data
-# number of years since 1982
-data["years_since_1982"] = [(d.year + d.timetuple().tm_yday / 365.25) - 1982 for d in data.date]
-
-# normalise temperatures
-data["normalised_temperature"] = (data["min_temperature"] - np.mean(data["min_temperature"])) / np.std(data["min_temperature"])
-
-
-# test:
-#print(data.head(5))
-
-# test plot:
-plt.scatter(data["years_since_1982"], data["normalised_temperature"], marker=".")
-plt.plot(data["years_since_1982"], model.predict(data["normalised_temperature"]), c="r")
-
-# labels and legend
-plt.xlabel("years_since_1982")
-plt.ylabel("normalised_temperature")
-plt.title("January Temperatures (Normalised)")
-
-plt.savefig("plots/scatter_plot.pdf")
+    # save plot as pdf
+    plt.savefig("plots/scatter_plot.pdf")
